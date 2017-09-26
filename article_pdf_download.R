4ffd9916430ea5351db054ce4c7bf43fe33a8b09
yourName <yourEmail>
2017-09-26 16:16:11 -0700

    code review

--- article_pdf_download.R
+++ article_pdf_download.R
@@ -2,7 +2,8 @@
 # PACKAGES
 # ===============================
 # library("devtools")
-# install_github("massimoaria/bibliometrix") # Needs to installed from GitHub not CRAN
+# install_github("massimoaria/bibliometrix") # Needs to be installed from GitHub not CRAN
+library(tools) # for file manipulations
 library(tidyverse) # for general purpose data manip
 library(bibliometrix) # For reading and analyzing ISI stuff
 library(crminer) # for getting texts from DOI
@@ -10,15 +11,28 @@ library(XML) # for parsing elsevier xml responses
 library(rvest) # for parsing html responses
 library(jsonlite) # for parsing json objects
 
+
 # ===============================
 # CONSTANTS
 # ===============================
 data_dir <- './data' # Relative path to repository. To be changed as needed
 isi_dir <- file.path(data_dir, 'isi_searches')
+
+# Create the main output directory
 output_dir <- file.path(data_dir, 'pdf_output')
+# Check if pdf_output directory exists
+dir.create(output_dir, showWarnings = FALSE)
+
+# PDF subdirectory
 pdf_output_dir <- file.path(output_dir, 'pdfs')
-# Check if outpath exists
+# Check if pdf_output_dir directory exists
 dir.create(pdf_output_dir, showWarnings = FALSE)
+
+# Non-PDF files subdirectory
+nopdf_output_dir <- file.path(output_dir, 'non-pdfs')
+# Check if pdf_output_dir directory exists
+dir.create(nopdf_output_dir, showWarnings = FALSE)
+
 
 
 # ===============================
@@ -86,53 +100,32 @@ is_binary <- function(filepath,max=1000){
 # ===============================
 
 ## STEP 1: READ DOIs/METADATA FROM BIB FILES
-# generalized the bib file read in process. adding a bib file to the pile requires no longer requires extra code; the userneed only add the file to the directory full of bib files
+# Combine all the bib files. The user needs only add the file to the directory full of bib files
 filepath_list <- as.list(file.path(isi_dir, dir(isi_dir)))
 file_list <- convert2df(do.call(readFiles, filepath_list))
-# the function call above is the same as the function call below-- just way more convenient and versatile
-# soil.health <- convert2df(readFiles(file.path(isi_dir, 'healthy.rangelands.bib'),
-#                                     file.path(isi_dir, 'rangeland_SAME_soil.health.bib'),
-#                                     file.path(isi_dir, 'rangeland.health.bib'),
-#                                     file.path(isi_dir, 'soil.health_healthy.soil_1to500.bib'),
-#                                     file.path(isi_dir, 'soil.health_healthy.soil_501to1000.bib'),
-#                                     file.path(isi_dir, 'soil.health_healthy.soil_1001to1463.bib'),
-#                                     file.path(isi_dir, 'soil.quality_1to500.bib'),
-#                                     file.path(isi_dir, 'soil.quality_501to1000.bib'),
-#                                     file.path(isi_dir, 'soil.quality_1001to1500.bib'),
-#                                     file.path(isi_dir, 'soil.quality_1501to2000.bib'),
-#                                     file.path(isi_dir, 'soil.quality_2001to2500.bib'),
-#                                     file.path(isi_dir, 'soil.quality_2501to3000.bib'),
-#                                     file.path(isi_dir, 'soil.quality_3001to3500.bib'),
-#                                     file.path(isi_dir, 'soil.quality_3501to4000.bib'),
-#                                     file.path(isi_dir, 'soil.quality_4001to4500.bib'),
-#                                     file.path(isi_dir, 'soil.quality_4501to5000.bib'),
-#                                     file.path(isi_dir, 'soil.quality_5001to5500.bib'),
-#                                     file.path(isi_dir, 'soil.quality_5501to6000.bib'),
-#                                     file.path(isi_dir, 'soil.quality_6001to6500.bib'),
-#                                     file.path(isi_dir, 'soil.quality_6501to7000.bib'),
-#                                     file.path(isi_dir, 'soil.quality_7001to7471.bib')
-# ))
 
 # removing duplicate records
 # NOTE: turns out that this still lets some duplicate files through (see: 'STEP 4: POST-PROCESSING')
-soil.health <- duplicatedMatching(soil.health,Field="TI") 
+# Pretty time consuming step
+soil.health <- duplicatedMatching(file_list, Field="TI") 
 
 
 ## STEP 2: ORGANIZE LINKS
 message('===============================\nORGANIZING LINKS\n===============================')
-my_df <- data.frame(paste(gsub(";.*$", "", soil.health$AU),soil.health$PY,soil.health$JI),soil.health$DI, stringsAsFactors = FALSE)
-names(my_df) <- c('Name','DOI')
+# Select attributes of interest and clean the author field
+my_df <- data.frame(Name=paste(gsub(";.*$", "", soil.health$AU),soil.health$PY,soil.health$JI),
+                    DOI=soil.health$DI, stringsAsFactors = FALSE)
 my_df <- my_df[!is.na(my_df$DOI),] # Of 8693 observations, 6406 have valid DOIs
-my_df <- my_df[1:50,]
 
 # collecting links
-system.time(my_df$links <- sapply(my_df$DOI, crm_links)) # getting links for each DOI
+# Note: This step takes ~20min to run 
+my_df$links <- sapply(my_df$DOI, crm_links) # getting links for each DOI
+# Remove references for which no DOI URL was found
 my_df <- my_df[lapply(my_df$links, length) > 0,] # 5759 of 6406 DOIs returned links via crm_links()
 
-
-# elsevier links require a separate dl process, so we distinguish them here
+# elsevier links require a separate download process, so we distinguish them here
 for (i in 1:length(my_df$links)) {
-  if (grepl('elsevier',my_df$link[[i]])) { # checking for string 'elsevier' in link
+  if (grepl('elsevier',my_df$links[[i]][[1]])) { # checking for string 'elsevier' in link
     my_df$elsevier[i] <- TRUE
   } else {
     my_df$elsevier[i] <- FALSE
@@ -160,7 +153,7 @@ message('===============================\nDOWNLOADING PDFS FROM LINKS\n=========
 
 # Here, I call the elsevier_pdf_download() function repeatedly via a loop that iterates through the rows of the dataframe created in the preceding
 # 'organize links' section of the script
-system.time(for (i in 1:dim(my_df)[1]) {
+for (i in 1:dim(my_df)[1]) {
   url <- my_df$download_link[i]
   my_df$path[i] <- paste0(file.path(pdf_output_dir, my_df$Name[i]), '.pdf')
   if (my_df$elsevier[i]) {
@@ -181,12 +174,12 @@ system.time(for (i in 1:dim(my_df)[1]) {
                                  finally = message(paste("\nProcessed URL:", url)))
   }
   message('[', i, '/', dim(my_df)[1], ']')
-})
+}
 
 message('===============================\nPDFS DOWNLOADED\n===============================')
 
 ## STEP 4: POST-PROCESSING
-# distinguish pdf files
+# distinguish real pdf files from other files (mainly html webpages)
 for (i in 1:dim(my_df)[1]) {
   if (file.exists(my_df$path[i])) {
     my_df$downloaded[i] <- TRUE
@@ -196,18 +189,25 @@ for (i in 1:dim(my_df)[1]) {
     my_df$is_pdf[i] <- FALSE
   }
 }
+# Add the flags for downloaded and PDF to the data frame
 my_df$downloaded <- as.logical(my_df$downloaded) 
 my_df$is_pdf <- as.logical(my_df$is_pdf)
 
-sum(my_df$downloaded) # out of 5759 acquired links, 4604 produced downloaded files
-length(unique(my_df$path[my_df$downloaded])) # out of 4604 downloaded files, 4539 are unique
-length(unique(my_df$path[my_df$downloaded & my_df$is_pdf])) # out of 4539 unique downloaded files, 4057 are binary files (PDFs)
+# Extract some statistics
+download_success <- sum(my_df$downloaded) # out of 5759 acquired links, 4604 produced downloaded files
+unique_files <- length(unique(my_df$path[my_df$downloaded])) # out of 4604 downloaded files, 4539 are unique
+unique_pdfs <- length(unique(my_df$path[my_df$downloaded & my_df$is_pdf])) # out of 4539 unique downloaded files, 4057 are binary files (PDFs)
+message(sprintf("Over the %i acquired links, %i PDFs were succesfully downloaded", nrow(my_df), unique_pdfs))
 
+# Extract the files info that were not PDFs
 non_pdf_paths <- unique(my_df$path[my_df$downloaded & !my_df$is_pdf]) # For investigative purposes, here are the paths for the non-PDF files (482) that were downloaded
 
-# removing non-pdf files
+# Move the non-pdf files
 for (non_pdf_path in non_pdf_paths) {
-  file.remove(non_pdf_path)
+  file.rename(from = non_pdf_path, 
+              to = file.path(nopdf_output_dir, 
+                           paste0(basename(file_path_sans_ext(non_pdf_path)),".html"))
+              )
 }
 
 # output information regarding the download processs to csv
